8615e793f151c92229a66279f67e9039c4659dc6
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2023-03-13 22:16:06 +0100

    lint and mypy cfg changes

--- src/adapters/get_block.py
+++ src/adapters/get_block.py
@@ -1,5 +1,5 @@
-import json
 from dataclasses import dataclass
+import json
 
 import requests
 
@@ -12,15 +12,17 @@ class GetBlock(Node):
     _api_key: str = "7203d8ab-29f8-46d1-b81a-4f1f0cea5b7a"
     _headers = {"x-api-key": _api_key, "Content-Type": "application/json"}
     _payload = {
-            "jsonrpc": "2.0",
-            "method": "eth_getBlockByNumber",
-            "params": [1, True],
-            "id": "getblock.io",
-        }
+        "jsonrpc": "2.0",
+        "method": "eth_getBlockByNumber",
+        "params": [1, True],
+        "id": "getblock.io",
+    }
 
     def get_block(self, block_num: int) -> dict:
         self._payload["params"] = [hex(block_num), False]
-        response = requests.post(self._url, headers=self._headers, data=json.dumps(self._payload))
+        response = requests.post(
+            self._url, headers=self._headers, data=json.dumps(self._payload)
+        )
         response_json = response.json()
         return response_json
 
