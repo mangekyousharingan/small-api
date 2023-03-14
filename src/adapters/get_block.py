0548a177bd8c81b330c640c227e41af270a61439
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2023-03-14 19:03:16 +0100

    api key

--- src/adapters/get_block.py
+++ src/adapters/get_block.py
@@ -9,7 +9,7 @@ from ports.node_provider import NodeProvider
 @dataclass
 class GetBlockIo(NodeProvider):
     _url: str = "https://eth.getblock.io/mainnet/"
-    _api_key: str = "7203d8ab-29f8-46d1-b81a-4f1f0cea5b7a"
+    _api_key: str = "7203d8ab-29f8-46d1-b81a-4f1f0cea5b7a"  # TODO: usually I am not pushing api keys to public repos :D
     _headers = {"x-api-key": _api_key, "Content-Type": "application/json"}
     _payload = {
         "jsonrpc": "2.0",
