3e0b696538163307178390402065fa128aec7191
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2023-03-14 17:12:10 +0100

    Update request & response models

--- src/core/models/requests.py
+++ src/core/models/requests.py
@@ -1,18 +1,18 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
-class BlocDataRequest:
-    _block_number: int
+class BlocInfoRequest:
+    _block_number: int = field(default_factory=int)
 
     @property
-    def block_number(self):
+    def block_number(self) -> int:
         return self._block_number
 
     @block_number.setter
-    def block_number(self, value):
+    def block_number(self, value: int) -> None:
         if not value > 0:
-            raise ValueError(f"Block number must be greater than {value}")
+            raise ValueError("Block number must be greater than 0")
         self._block_number = value
 
 
@@ -23,21 +23,21 @@ class SignaturesDataRequest:
     _page_size: int = 10
 
     @property
-    def page_number(self):
+    def page_number(self) -> int:
         return self._page_number
 
     @page_number.setter
-    def page_number(self, value):
+    def page_number(self, value: int) -> None:
         if not value > 0:
             raise ValueError(f"Page number must be greater than {value}")
         self._page_number = value
 
     @property
-    def page_size(self):
+    def page_size(self) -> int:
         return self._page_size
 
     @page_size.setter
     def page_size(self, value: int) -> None:
         if value not in range(1, 11):
-            raise ValueError(f"Page size must be in range 1-10")
+            raise ValueError("Page size must be in range 1-10")
         self.page_size = value
--- src/core/models/responses.py
+++ src/core/models/responses.py
@@ -2,12 +2,29 @@ from dataclasses import dataclass
 
 
 @dataclass
-class BlockDataResponse:
+class BlockInfoResponse:
     gas_limit: int
     gas_used: int
     number: int
     difficulty: int
     total_difficulty: int
+
+    def __init__(
+        self,
+        gas_limit: str,
+        gas_used: str,
+        number: str,
+        difficulty: str,
+        total_difficulty: str,
+    ):
+        self.gas_limit: int = self._convert(gas_limit)
+        self.gas_used: int = self._convert(gas_used)
+        self.number: int = self._convert(number)
+        self.difficulty: int = self._convert(difficulty)
+        self.total_difficulty: int = self._convert(total_difficulty)
+
+    def _convert(self, value: str) -> int:
+        return int(value, 16)
 
 
 @dataclass
