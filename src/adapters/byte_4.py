54b984b07128db2be0eb408ca9fae3116ad9c745
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2023-03-15 12:17:45 +0100

    Implement 4byte provider

--- src/adapters/byte_4.py
+++ src/adapters/byte_4.py
@@ -1,10 +1,19 @@
 from dataclasses import dataclass
 
+import requests
+
 from ports.signatures_provider import SignaturesProvider
 
 
 @dataclass
 class Byte4(SignaturesProvider):
-    def get_signatures(self) -> dict:
-        print("Calling 4bytes....")
-        return {}
+    _url = "https://www.4byte.directory/api/v1/signatures/"
+    _params = {}
+
+    def get_signatures(
+        self, hex_signature: str, page_size: int, page_number: int
+    ) -> dict:
+        self._params = {"hex_signature": hex_signature}
+        response = requests.get(self._url, params=self._params)
+        response_json = response.json()
+        return response_json
--- src/ports/signatures_provider.py
+++ src/ports/signatures_provider.py
@@ -5,5 +5,7 @@ from dataclasses import dataclass
 @dataclass
 class SignaturesProvider(ABC):
     @abstractmethod
-    def get_signatures(self) -> dict:
+    def get_signatures(
+        self, hex_signature: str, page_size: int, page_number: int
+    ) -> dict:
         pass
