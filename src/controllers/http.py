b870841b74b7c18cf763bef5e90f6bffd397a63c
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2023-03-15 12:18:56 +0100

    Implement signature info use case

--- src/controllers/http.py
+++ src/controllers/http.py
@@ -1,6 +1,7 @@
 import dataclasses
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 
 from core.models.requests import BlocInfoRequest, SignaturesInfoRequest
@@ -11,23 +12,34 @@ from core.usecases.get_signature_info import SignatureInfo
 def make_http_controller(
     block_info_usecase: BlockInfo, signature_info_usecase: SignatureInfo
 ) -> FastAPI:
-
     controller = FastAPI()
+
+    @controller.exception_handler(ValueError)
+    async def http_exception_handler(request: Request, exc: ValueError) -> JSONResponse:
+        print(type(request))
+        print(type(exc))
+        return JSONResponse(status_code=400, content={"error": str(exc)})
 
     @controller.get("/")
     def health() -> Response:
         return Response("OK")
 
-    @controller.get("/v1/blocks/{block_num}")
-    def blocks(block_num: int) -> JSONResponse:
+    @controller.get("/v1/blocks/{block_number}")
+    def blocks(
+        block_number: int = Path(description="The height of the block"),
+    ) -> JSONResponse:
         block_info_request = BlocInfoRequest()
-        block_info_request.block_number = block_num
+        block_info_request.block_number = block_number
         response = block_info_usecase(block_info_request)
         return JSONResponse(content=dataclasses.asdict(response))
 
     @controller.get("/v1/signatures/{signature}")
     def signatures(
-        signature: str, page_size: int = 10, page_number: int = 1
+        signature: str = Path(
+            description="ETH function hex signature", example="0x0bb1e8a0"
+        ),
+        page_size: int = Query(default=10, description="Page size in range 1-10"),
+        page_number: int = Query(default=1, description="Page number starting at 1"),
     ) -> JSONResponse:
         signature_info_request = SignaturesInfoRequest(signature)
         signature_info_request.page_size = page_size
--- src/core/usecases/get_signature_info/__init__.py
+++ src/core/usecases/get_signature_info/__init__.py
@@ -12,6 +12,20 @@ class SignatureInfo:
 
     def __call__(self, request: SignaturesInfoRequest) -> SignaturesInfoResponse:
         signatures_provider = self.provider_factory()
-        response_json = signatures_provider.get_signatures()
-        signatures_info_response = SignaturesInfoResponse([{"name": "_loan"}], 1, True)
+        response_json = signatures_provider.get_signatures(
+            request.hex_signature, request.page_size, request.page_number
+        )
+        signatures_info_response = self._prepare_response(response_json)
         return signatures_info_response
+
+    def _prepare_response(
+        self, signatures_provider_response: dict
+    ) -> SignaturesInfoResponse:
+        #  TODO:  one signature might have multiple names, adjust for pagination
+        results = signatures_provider_response["results"]
+        signature_info_response = SignaturesInfoResponse(
+            data=[{"name": data["text_signature"]} for data in results],
+            page_size=signatures_provider_response["count"],
+            is_last_page=True if not signatures_provider_response["next"] else False,
+        )
+        return signature_info_response
